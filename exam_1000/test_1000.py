8bb91fde7c8dcbf6636d759bc3259dfa01466398
rohawolf <rohawolf@gmail.com>
2019-01-08 14:39:40 +0900

    add exam_1000

---
+++ exam_1000/test_1000.py
@@ -0,0 +1,16 @@
+
+
+def range_checker(num):
+  return num if 0 < num < 10 else False
+
+if __name__ == '__main__':
+  a, b = map(int, input().split())
+
+  print('A : %s' % a)
+  print('B : %s' % b)
+
+  if not range_checker(a) or not range_checker(b):
+    print("the range of input must be between 0 and 10.")
+
+  else:
+    print('A + B = %s' % (a + b))
